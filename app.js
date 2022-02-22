e2b7e7eefd39efd24bd0587a513ab0495bb12dc8
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-02-22 20:57:02 +0100

    added cookie-parser env variable

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.use('/api', limiter);
 app.use(express.json({ limit: '10kb' }));
 
 // cookie parser middleware
-app.use(cookieParser('secret'));
+app.use(cookieParser(process.env.COOKIE_PARSER_SECRET));
 
 // data sanitization against NoSQL query injection
 app.use(mongoSanitize());
